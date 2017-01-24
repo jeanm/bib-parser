26252d411fb52589db3089624ccd916f63bf9ed5
Jean Maillard <jean@maillard.it>
2017-01-24 18:06:54 +0000

    Support "First Last" name format

--- src/parser/names.rs
+++ src/parser/names.rs
@@ -3,6 +3,37 @@ use pom::parser::*;
 use parser::{literal, sp0, sp1, msp0, msp1};
 use biblatex::{Name, NameList};
 
+// utility function for the `name()` parser; takes the list of tokens before
+// the comma, and optionally the list of tokens after the comma, and identifies
+// the given name and the family name
+fn split_name(mut before: Vec<String>, after: Option<Vec<String>>) -> Name {
+    match after {
+        None => match before.len() {
+            0 => unreachable!(),
+            // no commas, only one token
+            1 => {
+                Name {
+                    family: before.pop().unwrap(),
+                    .. Name::default()
+                }
+            }
+            // no commas, multiple tokens: last is family
+            _ => {
+                Name {
+                    family: before.pop().unwrap(),   
+                    given: Some(before.join(" ")),
+                }
+            }
+        },
+        Some(after) => {
+            Name {
+                family: before.join(" "),
+                given: Some(after.join(" ")),
+            }
+        }
+    }
+}
+
 // space-or-comma-delimited name token
 fn name_token() -> Parser<u8, String> {
     let simple = none_of(b" ,{}\t\n\r\\").repeat(1..).convert(|bs| String::from_utf8(bs));
@@ -11,16 +42,15 @@ fn name_token() -> Parser<u8, String> {
 }
 
 // comma-delimited name part
-fn name_part() -> Parser<u8, String> {
+fn name_part() -> Parser<u8, Vec<String>> {
     // TODO fix this ugly lookahead hack
-    let tokens = list(name_token(), sp1() - !((seq(b"and") - msp1())| seq(b"}")));
-    tokens.map(|ss| ss.join(" "))
+    list(name_token(), sp1() - !((seq(b"and") - msp1())| seq(b"}")))
 }
 
 // single name
 fn name() -> Parser<u8, Name> {
     let name = name_part() + (sp0() * sym(b',') * sp0() * name_part()).opt();
-    name.map(|(f, g)| Name { family: f, given: g })
+    name.map(|(b, a)| split_name(b, a))
 }
 
 /// List of names, used in fields such as `author` and `editor`.
@@ -36,15 +66,15 @@ mod test {
     use pom::DataInput;
 
     #[test]
-    fn part() {
-        let mut data = DataInput::new(b"'t  Hooft,junkjunkjunk");
-        let expected = "'t Hooft".to_string();
-        assert_eq!(name_part().parse(&mut data), Ok(expected));
-    }
-
-    #[test]
     fn single() {
         let mut data = DataInput::new(b"{'t Hooft}, Gerard and  junk");
+        let expected = Name {
+            family: "'t Hooft".to_string(),
+            given: Some("Gerard".to_string()),
+        };
+        assert_eq!(name().parse(&mut data), Ok(expected));
+
+        let mut data = DataInput::new(b"Gerard {'t Hooft}");
         let expected = Name {
             family: "'t Hooft".to_string(),
             given: Some("Gerard".to_string()),
@@ -67,13 +97,24 @@ mod test {
             truncated: false,
         };
 
-        let mut data1 = DataInput::new(b"{'t Hooft, Gerard and Celentano, A. Driano}");
-        assert_eq!(name_list().parse(&mut data1), Ok(expected.clone()));
+        // comma-separated
+        let mut data = DataInput::new(b"{'t Hooft, Gerard and Celentano, A. Driano}");
+        assert_eq!(name_list().parse(&mut data), Ok(expected.clone()));
 
-        let mut data2 = DataInput::new(b"{'t Hooft, Gerard and\n Celentano, A. Driano }");
-        assert_eq!(name_list().parse(&mut data2), Ok(expected.clone()));
+        // mixed
+        let mut data = DataInput::new(b"{'t Hooft, Gerard and A. Driano Celentano}");
+        assert_eq!(name_list().parse(&mut data), Ok(expected.clone()));
 
-        let mut data3 = DataInput::new(b"{'t Hooft, Gerard\n and Celentano, A. Driano }");
-        assert_eq!(name_list().parse(&mut data3), Ok(expected));
+        // no commas
+        let mut data = DataInput::new(b"{Gerard {'t Hooft} and A. Driano Celentano}");
+        assert_eq!(name_list().parse(&mut data), Ok(expected.clone()));
+
+        // newline
+        let mut data = DataInput::new(b"{Gerard {'t Hooft} and\n Celentano, A. Driano }");
+        assert_eq!(name_list().parse(&mut data), Ok(expected.clone()));
+
+        // weird spacing
+        let mut data = DataInput::new(b"{ 't Hooft,Gerard \n  and  Celentano  , A.  Driano }");
+        assert_eq!(name_list().parse(&mut data), Ok(expected));
     }
 }
